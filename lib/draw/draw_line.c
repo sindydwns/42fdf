3ab10167905cd390077a77e9331094ca4ecb0883
sindydwns <sindydwns@naver.com>
2022-11-30 12:12:56 +0900

    bresenham

--- lib/draw/draw_line.c
+++ lib/draw/draw_line.c
@@ -6,103 +6,46 @@
 /*   By: yonshin <yonshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 18:46:33 by yonshin           #+#    #+#             */
-/*   Updated: 2022/11/30 00:36:48 by yonshin          ###   ########.fr       */
+/*   Updated: 2022/11/30 12:16:21 by yonshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "draw.h"
+#include <math.h>
 
-static int	abs(int v)
+static int	abs(int value)
 {
-	if (v < 0)
-		return (-v);
-	return (v);
-}
-
-static int	draw_line_low(t_img *target, t_point start, t_point end, int color)
-{
-	int	dx;
-	int	dy;
-	int	yi;
-	int	d;
-	int	y;
-	int	x;
-
-	dx = end.x - start.x;
-	dy = end.y - start.y;
-	yi = 1;
-	if (dy < 0)
-	{
-		yi = -1;
-		dy = -dy;
-	}
-	d = (2 * dy) - dx;
-	y = start.y;
-	x = start.x;
-	while (x <= end.x)
-	{
-		draw_pixel(target, (t_point){x, y}, color);
-		if (d > 0)
-		{
-			y = y + yi;
-			d = d + (2 * (dy - dx));
-		}
-		else
-			d = d + 2 * dy;
-		x++;
-	}
-	return (0);
-}
-
-static int	draw_line_high(t_img *target, t_point start, t_point end, int color)
-{
-	int	dx;
-	int	dy;
-	int	xi;
-	int	d;
-	int	y;
-	int	x;
-
-	dx = end.x - start.x;
-	dy = end.y - start.y;
-	xi = 1;
-	if (dx < 0)
-	{
-		xi = -1;
-		dx = -dx;
-	}
-	d = (2 * dx) - dy;
-	x = start.x;
-	y = start.y;
-	while (y <= end.y)
-	{
-		draw_pixel(target, (t_point){x, y}, color);
-		if (d > 0)
-		{
-			x = x + xi;
-			d = d + (2  * (dx - dy));
-		}
-		else
-			d = d + 2 * dy;
-		y++;
-	}
-	return (0);
+	return (value * ((value > 0) - (value < 0)));
 }
 
 int	draw_line(t_img *target, t_point start, t_point end, int color)
 {
-	if (abs(end.x - start.x) > abs(end.y - start.y))
+	const int	dx = abs(end.x - start.x);
+	const int	sx = (start.x < end.x) - (start.x >= end.x);
+	const int	dy = -abs(end.y - start.y);
+	const int	sy = (start.y < end.y) - (start.y >= end.y);
+	int	error;
+
+	error = dx + dy;
+	while (start.x != end.x || start.y != end.y)
 	{
-		if (start.x < end.x)
-			return draw_line_low(target, start, end, color);
-		else
-			return draw_line_low(target, end, start, color);
+		draw_pixel(target, (t_point) {start.x, start.y}, color);
+		if (error * 2 >= dy)
+		{
+			if (start.x == end.x)
+				break;
+			error += dy;
+			start.x += sx;
+		}
+		if (error * 2 <= dx)
+		{
+			if (start.y == end.y)
+				break;
+			error += dx;
+			start.y += sy;
+		}
 	}
-	else
-	{
-		if (start.y < end.y)
-			return draw_line_high(target, start, end, color);
-		else
-			return draw_line_high(target, end, start, color);
-	}
+	if (start.x == end.x && start.y == end.y)
+		draw_pixel(target, (t_point) {start.x, start.y}, color);
+	return (0);
 }
