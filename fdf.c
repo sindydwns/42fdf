0e67c4bb6286b8660b13a94ec8ff0bdaf62cdc56
sindydwns <sindydwns@naver.com>
2022-12-27 00:53:35 +0900

    mandan

--- fdf.c
+++ fdf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fdf.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yonshin <yonshin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: yonshin <yonshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 15:48:53 by yonshin           #+#    #+#             */
-/*   Updated: 2022/12/23 19:09:34 by yonshin          ###   ########.fr       */
+/*   Updated: 2022/12/27 00:52:02 by yonshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,11 +54,6 @@ t_matrix4	get_rotate_matrix(t_vector3 v)
 		vect3(0, 0, 1)
 	};
 	const t_matrix3 rot = m3_mul_m3(rx, m3_mul_m3(ry, rz));
-	// const t_matrix3 rot = m3_mul_m3(rx, m3_mul_m3(rz, ry));
-	// const t_matrix3 rot = m3_mul_m3(ry, m3_mul_m3(rz, rx));
-	// const t_matrix3 rot = m3_mul_m3(ry, m3_mul_m3(rx, rz));
-	// const t_matrix3 rot = m3_mul_m3(rz, m3_mul_m3(rx, ry));
-	// const t_matrix3 rot = m3_mul_m3(rz, m3_mul_m3(ry, rx));
 
 	return ((t_matrix4){
 		(t_vector4){rot.v1.x, rot.v1.y, rot.v1.z, 0},
@@ -78,29 +73,51 @@ t_matrix4	get_scale_matrix(t_vector3 v)
 	});
 }
 
+void	init_camera(t_camera *cam)
+{
+	cam->pos = (t_vector3){-120, -120, -120};
+	// cam->rot = (t_vector3){atan(1 / sqrt(2)) * 180 / M_PI, 0, -45};
+	cam->rot = (t_vector3){-45, atan(1 / sqrt(2)) * 180 / M_PI, 0};
+	// cam->zoom = 5;
+	cam->zoom = 1;
+}
+
 int	render_frame(t_img *img, t_obj *map, t_camera *cam, t_extra *e)
 {
+	map->scl = (t_vector3){10, 10, 10};
+
 	cam->tpos = vsum3(vmul3(cam->tpos, DEFER), vmul3(cam->pos, 1 - DEFER));
 	cam->trot = vsum3(vmul3(cam->trot, DEFER), vmul3(cam->rot, 1 - DEFER));
 	cam->tzoom = cam->tzoom * DEFER + cam->zoom * (1 - DEFER);
 	
-	// t_matrix4 move = get_move_matrix(vrev3(cam->tpos));
-	// t_matrix4 rotate = get_rotate_matrix(vrev3(cam->trot));
-	// t_matrix4 scale = get_scale_matrix((t_vector3){cam->tzoom, cam->tzoom, cam->tzoom});
-	// t_matrix4 xxx = m4_mul_m4(scale, m4_mul_m4(move, rotate));
-
-	// TODO : 물체에 대한 회전을 연습한 다음 기저 벡터를 바꿔 다시 다시 회전시키고 이동해보며 축이 잘 바뀌었는지 확인하기
 	t_matrix4 o_move = get_move_matrix(map->pos);
 	t_matrix4 o_rotate = get_rotate_matrix(map->rot);
 	t_matrix4 o_scale = get_scale_matrix(map->scl);
-	t_matrix4 yyy = m4_mul_m4(o_scale, m4_mul_m4(o_move, o_rotate));
+	t_matrix4 obj_matrix = m4_mul_m4(o_move, m4_mul_m4(o_rotate, o_scale));
 
-	// t_matrix4 mat = m4_mul_m4(xxx, yyy);
-	t_matrix4 mat = yyy;
+	t_matrix4 move = get_move_matrix(vrev3(cam->tpos));
+	t_matrix4 rotate = m4_trans(get_rotate_matrix(cam->trot));
+	t_matrix4 scale = get_scale_matrix((t_vector3){cam->tzoom, cam->tzoom, cam->tzoom});
+	t_matrix4 cam_matrix = m4_mul_m4(scale, m4_mul_m4(rotate, move));
+	// t_matrix4 cam_matrix = m4_mul_m4(rotate, move);
+	
+	t_matrix4 axis = (t_matrix4){
+		{1, 0, 0, 0},
+		{0, 1, 0, 0},
+		{0, 0, 1, 0},
+		{0, 0, 0, 1}
+	};
+	t_matrix4 axis_cam_matrix = m4_mul_m4(axis, cam_matrix);
+	t_matrix4 mat = m4_mul_m4(axis_cam_matrix, obj_matrix);
+	// t_matrix4 mat = obj_matrix;
+	
+	printf("map pos: %.2f\t %.2f\t %.2f\t\n", map->pos.x, map->pos.y, map->pos.z);
+	printf("map rot: %.2f\t %.2f\t %.2f\t\n", map->rot.x, map->rot.y, map->rot.z);
+	printf("map scl: %.2f\t %.2f\t %.2f\t\n", map->scl.x, map->scl.y, map->scl.z);
+	printf("cam pos: %.2f\t %.2f\t %.2f\t\n", cam->pos.x, cam->pos.y, cam->pos.z);
+	printf("cam rot: %.2f\t %.2f\t %.2f\t\n", cam->rot.x, cam->rot.y, cam->rot.z);
+	printf("cam zoom: %.2f\n\n", cam->zoom);
 
-	// printf("%f %f %f\n", map->pos.x, map->pos.y, map->pos.z);
-	// printf("%f %f %f\n", map->rot.x, map->rot.y, map->rot.z);
-	// printf("%f %f %f\n\n", map->scl.x, map->scl.y, map->scl.z);
 	for (int i = 0; i < map->dot_len; i++) {
 		t_vector3 point = map->dots[i];
 		t_vector4 res = m4_mul_v4(mat, vect4(point.x, point.y, point.z, 1));
@@ -111,12 +128,22 @@ int	render_frame(t_img *img, t_obj *map, t_camera *cam, t_extra *e)
 		t_line line = map->lines[i]; 
 		t_vector3 s = map->d[line.s];
 		t_vector3 e = map->d[line.e];
-		if (s.z > 0 || e.z > 0)
+		if (s.z < 0 || e.z < 0)
 			continue;
 		draw_line(img, (t_point){s.x, s.y, C_YELLOW}, (t_point){e.x, e.y, C_YELLOW});
 	}
-	draw_line(img, (t_point){0, HEIGHT / 2, C_WHITE}, (t_point){WIDTH, HEIGHT / 2});
-	draw_line(img, (t_point){WIDTH / 2, 0, C_WHITE}, (t_point){WIDTH / 2, HEIGHT});
+	draw_line(img, (t_point){0, HEIGHT / 2, C_WHITE}, (t_point){WIDTH, HEIGHT / 2, C_WHITE});
+	draw_line(img, (t_point){WIDTH / 2, 0, C_WHITE}, (t_point){WIDTH / 2, HEIGHT, C_WHITE});
+	t_vector4 zero = vsum4(m4_mul_v4(axis_cam_matrix, vect4(0, 0, 0, 1)), (t_vector4){WIDTH / 2, HEIGHT / 2, 0, 0});
+	for (int i = -3; i < 3; i++)
+		for (int j = -3; j < 3; j++)
+			draw_pixel(img, (t_point){zero.x + i, zero.y + j, C_RED});
+	t_vector4 xx = vsum4(m4_mul_v4(axis_cam_matrix, vect4(1000, 0, 0, 1)), (t_vector4){WIDTH / 2, HEIGHT / 2, 0, 0});
+	draw_line(img, (t_point){xx.x, xx.y, C_RED}, (t_point){zero.x, zero.y, C_WHITE});
+	t_vector4 yy = vsum4(m4_mul_v4(axis_cam_matrix, vect4(0, 1000, 0, 1)), (t_vector4){WIDTH / 2, HEIGHT / 2, 0, 0});
+	draw_line(img, (t_point){yy.x, yy.y, C_GREEN}, (t_point){zero.x, zero.y, C_WHITE});
+	t_vector4 zz = vsum4(m4_mul_v4(axis_cam_matrix, vect4(0, 0, 1000, 1)), (t_vector4){WIDTH / 2, HEIGHT / 2, 0, 0});
+	draw_line(img, (t_point){zz.x, zz.y, C_BLUE}, (t_point){zero.x, zero.y, C_WHITE});
 	e++	;
 	return (0);
 }
@@ -124,30 +151,70 @@ int	render_frame(t_img *img, t_obj *map, t_camera *cam, t_extra *e)
 int	render_next_frame(t_data *data)
 {
 	data->frame++;
-	if (data->ex.key[KEY_W])
-		data->camera.pos.y -= 1;
-	if (data->ex.key[KEY_A])
-		data->camera.pos.x -= 1;
-	if (data->ex.key[KEY_S])
-		data->camera.pos.y += 1;
-	if (data->ex.key[KEY_D])
-		data->camera.pos.x += 1;
-	if (data->ex.key[KEY_R])
-		data->camera.pos.z += 1;
-	if (data->ex.key[KEY_F])
-		data->camera.pos.z -= 1;
-	if (data->ex.key[KEY_Q])
-		data->camera.rot.z += 1;
-	if (data->ex.key[KEY_E])
-		data->camera.rot.z -= 1;
-	if (data->ex.key[KEY_UP])
-		data->camera.rot.x += 1;
-	if (data->ex.key[KEY_DOWN])
-		data->camera.rot.x -= 1;
-	if (data->ex.key[KEY_LEFT])
-		data->camera.rot.y += 1;
-	if (data->ex.key[KEY_RIGHT])
-		data->camera.rot.y -= 1;
+	if (data->ex.key[KEY_SHIFT])
+	{
+		if (data->ex.key[KEY_W])
+			data->map->pos.y += 1;
+		if (data->ex.key[KEY_S])
+			data->map->pos.y -= 1;
+		if (data->ex.key[KEY_A])
+			data->map->pos.x -= 1;
+		if (data->ex.key[KEY_D])
+			data->map->pos.x += 1;
+		if (data->ex.key[KEY_R])
+			data->map->pos.z += 1;
+		if (data->ex.key[KEY_F])
+			data->map->pos.z -= 1;
+		if (data->ex.key[KEY_Q])
+			data->map->rot.z -= 1;
+		if (data->ex.key[KEY_E])
+			data->map->rot.z += 1;
+		if (data->ex.key[KEY_UP])
+			data->map->rot.x += 1;
+		if (data->ex.key[KEY_DOWN])
+			data->map->rot.x -= 1;
+		if (data->ex.key[KEY_LEFT])
+			data->map->rot.y -= 1;
+		if (data->ex.key[KEY_RIGHT])
+			data->map->rot.y += 1;
+	}
+	else
+	{
+		if (data->ex.key[KEY_1]) {
+			data->camera.pos.x -= 1;
+			data->camera.pos.y -= 1;
+			data->camera.pos.z -= 1;
+		}
+		if (data->ex.key[KEY_2]) {
+			data->camera.pos.x += 1;
+			data->camera.pos.y += 1;
+			data->camera.pos.z += 1;
+		}
+		if (data->ex.key[KEY_W])
+			data->camera.pos.y += 1;
+		if (data->ex.key[KEY_S])
+			data->camera.pos.y -= 1;
+		if (data->ex.key[KEY_A])
+			data->camera.pos.x -= 1;
+		if (data->ex.key[KEY_D])
+			data->camera.pos.x += 1;
+		if (data->ex.key[KEY_R])
+			data->camera.pos.z += 1;
+		if (data->ex.key[KEY_F])
+			data->camera.pos.z -= 1;
+		if (data->ex.key[KEY_Q])
+			data->camera.rot.z -= 1;
+		if (data->ex.key[KEY_E])
+			data->camera.rot.z += 1;
+		if (data->ex.key[KEY_UP])
+			data->camera.rot.x += 1;
+		if (data->ex.key[KEY_DOWN])
+			data->camera.rot.x -= 1;
+		if (data->ex.key[KEY_LEFT])
+			data->camera.rot.y -= 1;
+		if (data->ex.key[KEY_RIGHT])
+			data->camera.rot.y += 1;
+	}
 	mlx_mouse_get_pos(data->win, &data->ex.mouse.x, &data->ex.mouse.y);
 	ft_memset(data->img.addr, 0, WIDTH * HEIGHT * data->img.bits_per_pixel / 8);
 	render_frame(&data->img, data->map, &data->camera, &data->ex);
@@ -201,10 +268,7 @@ int	main(int argc, char *argv[])
 	if (data.mlx == 0)
 		exit(1);
 	data.map = create_map(argv[1]);
-	// data.camera.pos = (t_vector3){10, 10, 10};
-	data.camera.pos.z = 100000;
-	data.camera.rot = (t_vector3){-45, 0, atan(1 / sqrt(2)) * 180 / M_PI};
-	data.camera.zoom = 10;
+	init_camera(&data.camera);
 	data.win = mlx_new_window(data.mlx, WIDTH, HEIGHT, "fdf");
 	data.img.size = (t_point){WIDTH, HEIGHT, 0};
 	data.img.obj = mlx_new_image(data.mlx, data.img.size.x, data.img.size.y);
